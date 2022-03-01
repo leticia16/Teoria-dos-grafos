fc771ccc5bc36d66e79d35ef552f892a0a196522
Letícia Medeiros <leticialeca@hotmail.com>
2022-03-01 05:25:26 -0300

    Update dijkstra.cpp

--- dijkstra/dijkstra.cpp
+++ dijkstra/dijkstra.cpp
@@ -5,122 +5,23 @@ using namespace std;
 #include <stdio.h>
 #include <unistd.h>
 #include <string>
-ofstream fileOut;
+#include "../readFile.h"
+#include "../mainArgs.h"
 
 typedef pair<int, int> ii;
 
-// read the inFile
-const int INF = 1e9;
-ifstream inFile;
 string line_file;
-vector <int> numbers_line;
-int aux_v, aux_e;
-
-
-
 int nVertices, nEdges;
 
 vector<vector<ii>> adj;
 vector<int> dist;
 vector<bool> visited;
 
-
-//read the in File
-int concatNumber(int prev, int next){
-    string a = to_string(prev);
-    string b = to_string(next);
-    string number = a + b;
-    int num = stoi(number);
-    return num;
-}
-
-void linesReader (string line){
-    int j = 0, k = 0, prev = INF, curr = INF;
-    numbers_line.resize(3);
-    numbers_line.assign(3, 0);
-    line = line + " "; 
-   
-    for(int i = 0; i < line.length(); i++){
-       
-        if((int) line[i] == 32){
-            
-            if(curr == INF){ // caso o número seja de apenas 1 dígito
-                numbers_line[j] = prev;
-                j++; k = 0; prev = INF; curr = INF;
-                
-            }
-            //não sendo: 
-            else{
-            numbers_line[j] = concatNumber(prev, curr); //encontrou um número > 9
-            j++; k=0; prev = INF, curr = INF;
-            
-            }
-            
-        }
-        else if(k == 0){
-
-            if((int) line[i] == 45  && j==2){ // encontou um peso negativo.
-
-                if((int) line[i+2] != 32){ // peso negativo de 2 casas
-                    prev = ((int)line[i+1]-48);
-                    curr = ((int)line[i+2]-48);
-                    numbers_line[j] = - concatNumber(prev, curr);
-                    j++; k=0; prev = INF, curr = INF;
-                    i+=3;
-                    
-
-                }
-                else if((int) line[i+2] == 32){ // peso negativo de 1 casa
-                   
-                    prev = -((int)line[i+1] - 48);
-                    // cout << "PESO NEGATIVO 1 CASA: " << prev << "| line[ " << i+1 << " ] = " << (int) line[i+1] << "| (" << (int) line[i+1]-48 << ")\n";
-                    numbers_line[j] = prev;
-                    j++; k=0; curr = INF;
-                    i++;
-                }
-            }
-            else{
-                prev = (int) line[i] - 48;
-                k++;
-            }
-            
-            
-        }
-        else if(k==1){
-            curr = (int) line[i] - 48; // nesse caso não existe número negativo
-            
-        }
-    } 
-    return;
-}
-
-void read_v_e( string line){
-    int space, i = -1;
-    int curr = 0, prev = 0;
-
-    aux_v = (int) line[0]-48;
-    aux_e = (int) line[2]-48;
-    return;
-}
-
-
-
-
 void read(int v1, int v2, int w){
     v1--; v2--;
     
     adj[v1].push_back({v2, w});
     adj[v2].push_back({v1, w});
-
-    // for(int i = 0; i < nEdges; i++){
-    //     // cout << "i = " << i << "\n";
-    //     // cin >> v1 >> v2 >> w;
-    //     v1--; v2--;
-    //     adj[v1].push_back({v2, w});
-    //     adj[v2].push_back({v1, w});
-
-
-    // }
     return;
 }
 
@@ -186,9 +87,9 @@ void show(bool write_file, bool print_individual, int end){
 
 int main(int argc, char **argv){
 
-  bool flag_outFile = false, flag_print_individual = false, flag_write_in_file = false, flag_solution = false;
+//   bool flag_outFile = false, flag_print_individual = false, flag_write_in_file = false, flag_solution = false, flag_inFile = false;
   int root = 1, end = 0;
-  string nameFileOut, nameFileIn;
+//   string nameFileOut, nameFileIn;
   nameFileIn = " ";
   for(int i=1; i < argc; i++){ //starting from i=1 to skip the file path that's archived in argv[0]
         
@@ -196,14 +97,8 @@ int main(int argc, char **argv){
         string currArg = argv[i];
         
         if(currArg.compare("-h") == 0){
-
             //Help Options
-            cout << "-h : mostra o help\n";
-            cout << "-o <arquivo> : redireciona a saida para o 'arquivo' \n";
-            cout << "-f <arquivo> : indica o 'arquivo' que contém o grafo de entrada \n";
-            cout << "-s : mostra a solucao (em ordem crescente)\n";
-            cout << "-i : vertice inicial (dependendo do algoritmo)\n";
-            cout << "-l : vertice final (dependendo do algoritmo)\n";
+            showHelpOptions();
         }
 
         if (currArg.compare("-o") == 0){
@@ -215,7 +110,7 @@ int main(int argc, char **argv){
         }
 
         if(currArg.compare("-f") == 0){
-             nameFileIn = argv[i+1]; 
+            nameFileIn = argv[i+1]; 
             inFile.open(nameFileIn);
            
             if(inFile.is_open()){
@@ -227,8 +122,6 @@ int main(int argc, char **argv){
                         read_v_e(line_file);
                         nVertices = aux_v;
                         nEdges = aux_e;
-                        //debug
-                        // cout << "( " << nVertices << ", " << nEdges << " )\n";
                         adj.resize(nVertices);
                         visited.resize(nVertices);
                         dist.resize(nVertices);
@@ -237,10 +130,6 @@ int main(int argc, char **argv){
                     else{
                         linesReader(line_file);
                         read(numbers_line[0], numbers_line[1], numbers_line[2]);
-                        //debug
-                        // cout << "( " << numbers_line[0] << ") "<< "";  
-                        // cout << "( " << numbers_line[1] << ") "<< "";  
-                        // cout << "( " << numbers_line[2] << ") "<< "\n";  
                     }
                     i++;
                 }
@@ -249,6 +138,7 @@ int main(int argc, char **argv){
                 cout<< "Não foi possível abrir o arquivo.";
             }
             i++;
+            flag_inFile = true;
         }
 
         if(currArg.compare("-s") == 0){
@@ -270,16 +160,14 @@ int main(int argc, char **argv){
             i++;
         }
     }
-    if(nameFileIn == " "){
-        cout << "| ERRO 001 (arquivo de entrada faltante): \n";
-        cout << "| Voce precisa indicar o arquivo de entrada no qual esta contido o grafo a ser executado! \n| Por favor, execute novamente com o comando -f <arquivo> para que o algoritmo possa ser executado corretamente.\n| Ou acesse a central de ajuda com o comando -h\n";
+    if(nameFileIn == " " && flag_inFile == true ){
+        error_001();
     }
     else{
         dijkstra(root-1);
         inFile.close();
         
     }
-
     if(flag_outFile){
         show(true, flag_print_individual, end-1);
         cout << "| Arquivo <" << nameFileOut <<"> gerado com sucesso!";
@@ -287,12 +175,6 @@ int main(int argc, char **argv){
     }else if(flag_solution){
         show(false,flag_print_individual,end-1);
     }
-    else{
-        show(false, flag_print_individual, end-1);
-    }
 
 return 0;
 }
-
-
-
